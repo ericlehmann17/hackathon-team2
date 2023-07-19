4e83037e88ee44c8bde5ff2aa186836a9451e2a2
ericlehmann <lehmann.eric.j@gmail.com>
2023-07-19 15:37:49 -0400

    feat: dump/load tests + prediction tests

--- ml/src/train.py
+++ ml/src/train.py
@@ -5,7 +5,8 @@ TMR hackathon team 2
 """
 
 from sklearn.neural_network import MLPClassifier
-from joblib import dump
+from joblib import dump, load
+from pandas import read_csv
 import os
 
 ## input - preprocessed user browsing data
@@ -15,9 +16,15 @@ import os
 if __name__ == '__main__':
 
     ## TODO: load preprocessed data here
-    df = read_csv('ProcessedHackathonMockDataDraft.csv', header=0)
-    df.drop('Employee ID')
-    X, y = df.drop('Recommended page', axis=1), df['Recommended page']
+    ## Training data
+    df_train = read_csv('data/processed/train.csv', header=0)
+    df_train = df_train.drop('Employee ID', axis=1)
+    X_train, y_train = df_train.drop('Recommended page', axis=1), df_train['Recommended page']
+
+    ## Testing data
+    df_test = read_csv('data/processed/test.csv', header=0)
+    df_test = df_test.drop('Employee ID', axis=1)
+    X_test, y_test = df_test.drop('Recommended page', axis=1), df_test['Recommended page']
 
     ## parameters:
     ## - max iterations
@@ -28,10 +35,20 @@ if __name__ == '__main__':
     ## initialize the model
     nn = MLPClassifier(max_iter=max_iter, random_state=random_state)
     ## TODO: uncomment below once we have X and y
-    nn.fit(X, y)
+    nn.fit(X_train, y_train)
 
     ## now, dump to a file
     os.makedirs("models", exist_ok=True)
     dump(nn, 'models/trained_network.joblib')
 
-    ## to load, use load('/../models/trained_network.joblib')
+    ## to load, use load('models/trained_network.joblib')
+    nn_deserialized = load('models/trained_network.joblib')
+
+    ## test network
+    nn_deserialized.predict(X_train)
+    
+    ## show accuracy
+    print(nn_deserialized.score(X_test, y_test))
+
+    ## show log of probability outputs
+    print(nn_deserialized.predict_log_proba(X_test))
