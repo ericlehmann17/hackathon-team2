800c5713c6bb4b419cf00fde19f2b62c2d694991
Tate Rosen <84721398+taterosen@users.noreply.github.com>
2023-07-19 14:11:22 -0400

    Update train.py

--- ml/src/train.py
+++ ml/src/train.py
@@ -1,56 +1,37 @@
 """
-Skeleton code for random forest
+Skeleton code for neural net
 
 TMR hackathon team 2
 """
 
-import pandas as pd
-import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report,confusion_matrix, accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from joblib import dump
+import os
 
-#Reading the data
-employee_data = pd.read_csv('HackathonMockDataDraft.csv')
+## input - preprocessed user browsing data
+## output - serialized trained model to be sent to the decision-making service
 
 
-#Cleaning non-predictive attributes
-employee_data.drop(['Employee ID'], axis=1)
-#print(employee_data.head())
+if __name__ == '__main__':
 
-#Digitizing the data with one hot encoding
-one_hot_data = pd.get_dummies(employee_data[['Role','Hourly/Salary','Badges', 'Visited page']],drop_first=True)
-#print(one_hot_data.head())
+    ## TODO: load preprocessed data here
+    df = read_csv('ProcessedHackathonMockDataDraft.csv', header=0)
+    df.drop('Employee ID')
+    X, y = df.drop('Recommended page', axis=1), df['Recommended page']
 
-frames = [one_hot_data, employee_data[['# of Badges','Yeats at Company']]]
-final_df = pd.concat(frames, axis=1)
-#print(final_df.head())
+    ## parameters:
+    ## - max iterations
+    max_iter = 300
+    ## - random state. basically seed for bootstrapping
+    random_state = 1
 
+    ## initialize the model
+    nn = MLPClassifier(max_iter=max_iter, random_state=random_state)
+    ## TODO: uncomment below once we have X and y
+    nn.fit(X, y)
 
-#Creating X and y
-X = final_df.drop('Recommended page', axis=1)
-y = final_df['Recommended page']
+    ## now, dump to a file
+    os.makedirs("models", exist_ok=True)
+    dump(nn, 'models/trained_network.joblib')
 
-#Creating training and testing set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=11)
-
-
-#Making an object for RFC
-rfc = RandomForestClassifier(n_estimators=100,criterion="entropy",max_features="sqrt")
-
-#Fitting the model
-rfc.fit(X_train, y_train.values.ravel())
-
-#Predicting on test set
-rfc_pred = rfc.predict(X_test)
-
-#Printing the confusion matrix
-print(confusion_matrix(y_test,rfc_pred))
-
-#Printing the classification report
-print(classification_report(y_test,rfc_pred))
-
-#Printing the accuracy score
-print(accuracy_score(y_test,rfc_pred))
-
-
+    ## to load, use load('/../models/trained_network.joblib')
