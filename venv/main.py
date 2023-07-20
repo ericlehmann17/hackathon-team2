203c975c7a7e9be265d2845928aecc2e37ac5888
ericlehmann <lehmann.eric.j@gmail.com>
2023-07-20 18:31:30 -0400

    feat: recommendPages endpoint configured with hardcoded user

--- venv/main.py
+++ venv/main.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify, request
-
+from joblib import load
+from numpy import Infinity
+from sklearn.neural_network import MLPClassifier
 app = Flask(__name__)
+
+## deserialized ML model
+nn = load("ml/models/trained_network.joblib")
 
 incomes = [
     { 'description': 'salary', 'amount': 5000 }
@@ -19,9 +24,29 @@ def default_recommended_pages():
 def recommended_pages():
     request.get_json()
 
-    # ML code stuff
+    ## for now, user is hardcoded
+    user = [0.2,0.10344827586206896,7,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,1]
+    ## get prediction vector
+    prediction = list(nn.predict_log_proba([user])[0])
 
-    return [1,2,3,4,5,6,7,8], 200 #mock return in place of ML 
+    ## get recommendations
+    recs = []
+    for i in range(3):
+        max = -Infinity
+        max_index = None
+        for j in range(len(prediction)):
+            probability = prediction[j]
+            if probability > max:
+                max = probability
+                max_index = j
+        ## if no index of max, then the list is empty, so break loop
+        if max_index == None:
+            break
+        ## add recommendation, and repeat to find others
+        recs.append(max_index)
+        prediction.pop(max_index)
+
+    return recs, 200 #mock return in place of ML 
 
 @app.route('/addEmployee', methods=['POST'])
 def add_empoloyee():
